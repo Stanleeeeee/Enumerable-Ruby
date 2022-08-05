26a10ad53ba28aa3fbab755b63533f418fc22114
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-08-05 15:48:41 +0100

    Add corrections to rubocop linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,21 +1,21 @@
 module MyEnumerable
-    def all?
+  def all?
       return true unless block_given?
-  
+
       each { |n| return false unless yield n }
       true
     end
-  
-    def any?
+
+  def any?
       return true unless block_given?
-  
+
       each { |n| return true if yield n }
       false
     end
-  
-    def filter
+
+  def filter
       num_arr = []
       each { |n| num_arr.push(n) if yield n }
       num_arr
     end
-  end
+end
